9809245a747ae0069f38ef868b3b8a1093827424
Fireguy <nickname160504@yandex.ru>
2021-04-20 22:59:11 +0500

    Structure template added

---
+++ main.py
@@ -0,0 +1,17 @@
+from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import CallbackContext, CommandHandler
+
+from Functions import start
+from info import TOKEN
+
+
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.text, start.start))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
